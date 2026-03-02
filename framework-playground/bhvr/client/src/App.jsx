3b18d8459c0e6efb436622b5bd9e05260e653a52
kvilmos <kvilmos47@gmail.com>
2026-03-02 11:03:38 +0000

    feat(bhvr-stack): add external ui library

--- framework-playground/bhvr/client/src/App.jsx
+++ framework-playground/bhvr/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { API_URL } from "@app/shared";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [data, setData] = useState(null);
@@ -14,6 +15,16 @@ function App() {
   return (
     <div>
       <h1>server status: {data ? data : "loading..."}</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant="destructive" size="lg">
+          large red button
+        </Button>
+        <Button variant="link" size="lg">
+          large link button
+        </Button>
+        <Button>basic button</Button>
+        <Button variant="outline">outline button</Button>
+      </div>
     </div>
   );
 }
